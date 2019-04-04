dcf52f8277cbb94f68214552bacfd932cd4a89f1
semenov <semenov@skeeks.com>
2019-04-05 00:45:38 +0300

    dev

---
+++ src/views/widgets/ContentElementsCmsWidget/product-stick-item.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 06.03.2015
+ *
+ * @var \v3toys\skeeks\models\V3toysProductContentElement $model
+ *
+ */
+/* @var $this yii\web\View */
+//$shopProduct = \skeeks\cms\shop\models\ShopProduct::getInstanceByContentElement($model);
+$shopProduct = $model->shopProduct;
+
+$count = $model->relatedPropertiesModel->getSmartAttribute('reviews2Count');
+$rating = $model->relatedPropertiesModel->getSmartAttribute('reviews2Rating');
+//$v3ProductElement = new \v3toys\parsing\models\V3toysProductContentElement($model->toArray());
+$priceHelper = \Yii::$app->shop->cart->getProductPriceHelper($model);
+
+?>
+<div class="js-slide">
+    <div class="g-px-10">
+        <!-- Product -->
+        <figure class="g-pos-rel g-mb-10">
+            <a class="" href="<?= $model->url; ?>" title="<?= $model->name; ?>">
+                <img class="img-fluid" src="<?= \Yii::$app->imaging->thumbnailUrlOnRequest($model->image ? $model->image->src : null,
+                    new \skeeks\cms\components\imaging\filters\Thumbnail([
+                        'w' => 260,
+                        'h' => 200,
+                        'm' => \Imagine\Image\ImageInterface::THUMBNAIL_INSET,
+                    ]), $model->code
+                ); ?>" alt="<?= $model->name; ?>">
+
+            </a>
+            <!--<figcaption class="w-100 g-bg-primary g-bg-black--hover text-center g-pos-abs g-bottom-0 g-transition-0_2 g-py-5">
+                <a class="g-color-white g-font-size-11 text-uppercase g-letter-spacing-1 g-text-underline--none--hover" href="#!">New Arrival</a>
+            </figcaption>-->
+        </figure>
+
+        <div class="media text-center">
+            <!-- Product Info -->
+            <div class="d-flex flex-column">
+                <? if ($priceHelper->hasDiscount) : ?>
+                    <div class="old"><?= $priceHelper->basePrice->money; ?></div>
+                    <span class="d-block g-color-primary g-font-size-20"><?= $priceHelper->minMoney; ?></span>
+                <? else : ?>
+                    <span class="d-block g-color-primary g-font-size-20"><?= $priceHelper->minMoney; ?></span>
+                <? endif; ?>
+
+
+                <h4 class="h6 g-color-black mb-1">
+                    <a class="u-link-v5 g-color-black g-color-primary--hover" href="<?= $model->url; ?>" title="<?= $model->name; ?>">
+                        <?= $model->name; ?>
+                    </a>
+                </h4>
+                <? if ($model->cmsTree) : ?>
+                    <a class="d-inline-block g-color-gray-dark-v5 g-font-size-13" href="<?= $model->cmsTree->url; ?>"><?= $model->cmsTree->name; ?></a>
+                <? endif; ?>
+
+            </div>
+            <!-- End Product Info -->
+
+            <!-- Products Icons -->
+            <!--<ul class="list-inline media-body text-right">
+                <li class="list-inline-item align-middle mx-0">
+                    <a class="u-icon-v1 u-icon-size--sm g-color-gray-dark-v5 g-color-primary--hover g-font-size-15 rounded-circle" href="#!"
+                       data-toggle="tooltip"
+                       data-placement="top"
+                       title="Add to Cart">
+                        <i class="icon-finance-100 u-line-icon-pro"></i>
+                    </a>
+                </li>
+                <li class="list-inline-item align-middle mx-0">
+                    <a class="u-icon-v1 u-icon-size--sm g-color-gray-dark-v5 g-color-primary--hover g-font-size-15 rounded-circle" href="#!"
+                       data-toggle="tooltip"
+                       data-placement="top"
+                       title="Add to Wishlist">
+                        <i class="icon-medical-022 u-line-icon-pro"></i>
+                    </a>
+                </li>
+            </ul>-->
+            <!-- End Products Icons -->
+        </div>
+        <!-- End Product -->
+    </div>
+</div>
---
+++ src/views/widgets/ContentElementsCmsWidget/products-stick.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 25.05.2015
+ */
+/* @var $this   yii\web\View */
+/* @var $widget \skeeks\cms\cmsWidgets\contentElements\ContentElementsCmsWidget */
+$query = $widget->dataProvider->query;
+?>
+<? if ($query->count()) : ?>
+
+    <? if ($widget->label) : ?>
+        <div class="text-center mx-auto g-max-width-600 g-mb-20">
+            <h2 class="g-color-gray-dark-v2 mb-4"><?= $widget->label; ?></h2>
+            <!--<p class="lead">We want to create a range of beautiful, practical and modern outerwear that doesn't cost the earth – but let's you still live life in style.</p>-->
+        </div>
+    <? endif; ?>
+
+    <? echo \yii\widgets\ListView::widget([
+        'dataProvider' => $widget->dataProvider,
+        'itemView'     => 'product-stick-item',
+        'emptyText'    => '',
+        'options'      => [
+            'class' => 'js-carousel g-pb-0 g-mx-minus-10',
+            'tag'   => 'div',
+            'data'  => [
+                'slides-show'         => 6,
+                'arrows-classes'      => "u-arrow-v1 g-absolute-centered--y g-width-45 g-height-45 g-font-size-30 g-color-gray-dark-v5 g-color-primary--hover rounded-circle",
+                'arrow-left-classes'  => "fa fa-angle-left g-left-minus-40",
+                'arrow-right-classes' => "fa fa-angle-right g-right-minus-40",
+            ],
+        ],
+        'itemOptions'  => [
+            'tag' => false,
+        ],
+        'layout'       => '{items}',
+    ]) ?>
+
+<? endif; ?>
