d9190eaff2951b980f2c4d2e0d99482c64e23c46
semenov <semenov@skeeks.com>
2019-07-19 10:46:47 +0300

    dev

--- src/views/modules/cms/tree/home.php
+++ src/views/modules/cms/tree/home.php
@@ -8,29 +8,48 @@
 /* @var $this yii\web\View */
 ?>
 <?
+
 $catalog = \skeeks\cms\models\CmsTree::find()->where([
-    'dir' => 'catalog'
+    'dir' => 'catalog',
 ])->andWhere([
-    '>', 'image_id', 0
+    '>',
+    'image_id',
+    0,
 ])->one();
-echo \skeeks\cms\cmsWidgets\treeMenu\TreeMenuCmsWidget::widget([
-    'namespace'       => 'home-tree-slider',
-    'enabledRunCache'                => "Y",
-    'viewFile'        => '@app/views/widgets/TreeMenuCmsWidget/revolution-slider',
-    'treePid'         => $catalog ? $catalog->id : null,
-    //'enabledRunCache' => \skeeks\cms\components\Cms::BOOL_N,
-]);
+if (\Yii::$app->mobileDetect->isMobile) {
+
+    $widget = \skeeks\cms\cmsWidgets\treeMenu\TreeMenuCmsWidget::begin([
+        'namespace'       => 'mobile-home-catalog-small',
+        'viewFile'        => '@app/views/widgets/TreeMenuCmsWidget/sub-catalog-small',
+        'treeParentCode'         => "catalog",
+        'enabledRunCache' => \skeeks\cms\components\Cms::BOOL_N,
+    ]);
+    $widget->activeQuery->with('image');
+    \skeeks\cms\cmsWidgets\treeMenu\TreeMenuCmsWidget::end();
+} else {
+
+
+    echo \skeeks\cms\cmsWidgets\treeMenu\TreeMenuCmsWidget::widget([
+        'namespace'       => 'home-tree-slider',
+        'enabledRunCache' => "Y",
+        'viewFile'        => '@app/views/widgets/TreeMenuCmsWidget/revolution-slider',
+        'treeParentCode'         => "catalog",
+        //'enabledRunCache' => \skeeks\cms\components\Cms::BOOL_N,
+    ]);
+}
+
+
 ?>
 
 
-<? if (\Yii::$app->shop->shopContents) : ?>
+<? if (\Yii::$app->shop->shopContents && \Yii::$app->mobileDetect->isDesktop) : ?>
     <div class="container g-mt-40 g-mb-40">
         <?
         $widgetElements = \skeeks\cms\cmsWidgets\contentElements\ContentElementsCmsWidget::beginWidget("home-poupular-products", [
             'viewFile'             => '@app/views/widgets/ContentElementsCmsWidget/products-stick',
             'label'                => "Популярные товары",
             'enabledPaging'        => "N",
-            'enabledRunCache'                => "Y",
+            'enabledRunCache'      => "Y",
             'content_ids'          => \yii\helpers\ArrayHelper::map(\Yii::$app->shop->shopContents, 'id', 'id'),
             'limit'                => 15,
             'contentElementClass'  => \skeeks\cms\shop\models\ShopCmsContentElement::class,
@@ -53,7 +72,7 @@ echo \skeeks\cms\cmsWidgets\treeMenu\TreeMenuCmsWidget::widget([
             'viewFile'             => '@app/views/widgets/ContentElementsCmsWidget/products-stick',
             'label'                => "Новые поступления",
             'enabledPaging'        => "N",
-            'enabledRunCache'                => "Y",
+            'enabledRunCache'      => "Y",
             'content_ids'          => \yii\helpers\ArrayHelper::map(\Yii::$app->shop->shopContents, 'id', 'id'),
             'limit'                => 15,
             'contentElementClass'  => \skeeks\cms\shop\models\ShopCmsContentElement::class,
@@ -63,9 +82,9 @@ echo \skeeks\cms\cmsWidgets\treeMenu\TreeMenuCmsWidget::widget([
                 $activeDataProvider->query->with('shopProduct.baseProductPrice');
                 $activeDataProvider->query->with('shopProduct.minProductPrice');
                 $activeDataProvider->query->with('image');
-                if (!\Yii::$app->shop->is_show_product_no_price)   {
+                if (!\Yii::$app->shop->is_show_product_no_price) {
                     $activeDataProvider->query->joinWith('shopProduct.shopProductPrices as pricesFilter');
-                    $activeDataProvider->query->andWhere(['>','`pricesFilter`.price',0]);
+                    $activeDataProvider->query->andWhere(['>', '`pricesFilter`.price', 0]);
                 }
 
                 //$activeDataProvider->query->joinWith('shopProduct.baseProductPrice as basePrice');
@@ -78,34 +97,34 @@ echo \skeeks\cms\cmsWidgets\treeMenu\TreeMenuCmsWidget::widget([
 
 <? endif; ?>
 <? if ($model->description_full) : ?>
-<!-- What People Say -->
-<section class="container g-pt-10 g-pb-10">
-    <div class="row justify-content-between">
-        <div class="col-lg-12 flex-lg-unordered g-mt-50--lg g-mb-60">
-            <div class="mb-2">
-                <div class="d-inline-block g-width-20 g-height-2 g-pos-rel g-top-minus-4 g-bg-primary mr-2"></div>
-                <span class="g-color-gray-dark-v3 g-font-weight-600 g-font-size-12 text-uppercase">О компании</span>
+    <!-- What People Say -->
+    <section class="container g-pt-10 g-pb-10">
+        <div class="row justify-content-between">
+            <div class="col-lg-12 flex-lg-unordered g-mt-20--lg g-mb-20">
+                <div class="mb-2">
+                    <div class="d-inline-block g-width-20 g-height-2 g-pos-rel g-top-minus-4 g-bg-primary mr-2"></div>
+                    <span class="g-color-gray-dark-v3 g-font-weight-600 g-font-size-12 text-uppercase">О компании</span>
+                </div>
+
+                <?= $model->description_full; ?>
+
             </div>
-
-            <?=$model->description_full; ?>
-
         </div>
-    </div>
-</section>
-<!-- End What People Say -->
+    </section>
+    <!-- End What People Say -->
 <? endif; ?>
 <!-- Blog News -->
 <section class="container-fluid g-pt-10 g-pb-10">
     <?
     $widgetElements = \skeeks\cms\cmsWidgets\contentElements\ContentElementsCmsWidget::beginWidget("home-news", [
-        'viewFile'    => '@app/views/widgets/ContentElementsCmsWidget/news-masonry',
-        'label'                => "Новости компании",
-        'enabledRunCache'                => "Y",
-        'content_ids'          => [1],
-        'enabledPaging'       => 'N',
-        'enabledCurrentTree'   => \skeeks\cms\components\Cms::BOOL_N,
-        'enabledCurrentTreeChild' => skeeks\cms\components\Cms::BOOL_N,
-        'enabledCurrentTreeChildAll' => skeeks\cms\components\Cms::BOOL_N
+        'viewFile'                   => '@app/views/widgets/ContentElementsCmsWidget/news-masonry',
+        'label'                      => "Новости компании",
+        'enabledRunCache'            => "Y",
+        'content_ids'                => [1],
+        'enabledPaging'              => 'N',
+        'enabledCurrentTree'         => \skeeks\cms\components\Cms::BOOL_N,
+        'enabledCurrentTreeChild'    => skeeks\cms\components\Cms::BOOL_N,
+        'enabledCurrentTreeChildAll' => skeeks\cms\components\Cms::BOOL_N,
     ]);
     $widgetElements::end();
     ?>
