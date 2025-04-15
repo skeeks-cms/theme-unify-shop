2f9df7069c028309d96b49857eebfd9cfb08d63f
Semenov <semenov@skeeks.com>
2025-04-15 12:09:57 +0300

    Фиксация фильтров слева

--- src/views/modules/cms/tree/catalogs/_left-col.php
+++ src/views/modules/cms/tree/catalogs/_left-col.php
@@ -32,9 +32,9 @@ CSS
     ?>
 
 
-        <?
-        //Если есть дочерние разделы или есть родительские больше чем главная
-        if ($model->activeChildren || count($model->parents) > 1) : ?>
+    <?
+    //Если есть дочерние разделы или есть родительские больше чем главная
+    if ($model->activeChildren || count($model->parents) > 1) : ?>
 
         <?php
         $model = clone \Yii::$app->cms->currentTree;
@@ -42,24 +42,23 @@ CSS
 
         $data = [];
 
-        foreach ($model->parents as $parent)
-        {
-            if($parent->level > 0) {
+        foreach ($model->parents as $parent) {
+            if ($parent->level > 0) {
                 $data[] = [
-                    'tree' => $parent,
-                    'isParent' => $parent
+                    'tree'     => $parent,
+                    'isParent' => $parent,
                 ];
             }
         }
 
         if ($savedFilter) {
             $data[] = [
-                'tree' => $savedFilter->cmsTree,
-                'isParent' => $parent
+                'tree'     => $savedFilter->cmsTree,
+                'isParent' => $parent,
             ];
             $data[] = [
                 'savedFilter' => $savedFilter,
-                'isCurrent' => true,
+                'isCurrent'   => true,
             ];
 
             /*if ($model->activeChildren) {
@@ -75,56 +74,51 @@ CSS
 
             if ($appliedValues) {
                 $data[] = [
-                    'tree' => $model,
+                    'tree'     => $model,
                     'isParent' => true,
                 ];
                 $data[] = [
-                    'name' => "Применены фильтры",
+                    'name'      => "Применены фильтры",
                     'isCurrent' => true,
                 ];
             } else {
                 $data[] = [
-                    'tree' => $model,
+                    'tree'      => $model,
                     'isCurrent' => true,
                 ];
 
                 if ($model->activeChildren) {
-                    foreach ($model->activeChildren  as $child)
-                    {
+                    foreach ($model->activeChildren as $child) {
                         $data[] = [
-                            'tree' => $child,
-                            'isChildren' => true
+                            'tree'       => $child,
+                            'isChildren' => true,
                         ];
                     }
                 }
             }
 
 
-
         }
-
 
 
         ?>
 
 
-
-
-        <?php if($data) : ?>
-                <div class="sx-col-left-block">
+        <?php if ($data) : ?>
+            <div class="sx-col-left-block">
 
             <!--<div class="g-mb-10">
                 <div class="h5 sx-col-left-title">
-                    <?/*= \Yii::t('skeeks/unify', 'Categories'); */?>
+                    <? /*= \Yii::t('skeeks/unify', 'Categories'); */ ?>
                 </div>
             </div>-->
 
             <ul class="list-unstyled mb-0 sx-col-menu">
-            <? foreach ($data as $counter => $row) : ?>
-                <?php
-                /**
-                 * @var $savedFilter \skeeks\cms\models\CmsSavedFilter
-                 */
+                <? foreach ($data as $counter => $row) : ?>
+                    <?php
+                    /**
+                     * @var $savedFilter \skeeks\cms\models\CmsSavedFilter
+                     */
                     $name = \yii\helpers\ArrayHelper::getValue($row, "name");
                     $cmsTree = \yii\helpers\ArrayHelper::getValue($row, "tree");
                     $isCurrent = \yii\helpers\ArrayHelper::getValue($row, "isCurrent");
@@ -137,61 +131,59 @@ CSS
                     } elseif ($isParent) {
                         $title = "Смотреть все товары из раздела «{$cmsTree->seoName}»";
                     }
-                ?>
-                <?php if($cmsTree) : ?>
-                    <li class="<?php echo $isChildren ? "sx-children-cat" : ""; ?>">
-                        <? if($isCurrent) : ?>
-                            <div class="sx-main-text-color g-text-underline--none--hover btn btn-primary btn-block"
-                               data-toggle="tooltip" title="<?php echo $title; ?>"
+                    ?>
+                    <?php if ($cmsTree) : ?>
+                        <li class="<?php echo $isChildren ? "sx-children-cat" : ""; ?>">
+                            <? if ($isCurrent) : ?>
+                                <div class="sx-main-text-color g-text-underline--none--hover btn btn-primary btn-block"
+                                     data-toggle="tooltip" title="<?php echo $title; ?>"
+                                >
+                                    <?php if ($isParent) : ?>
+                                        <i class="hs-icon hs-icon-arrow-left"></i>
+                                    <?php endif; ?>
+                                    <?= $cmsTree->name; ?>
+                                </div>
+                            <? else : ?>
+                                <a class="sx-main-text-color g-text-underline--none--hover"
+                                   data-toggle="tooltip" title="<?php echo $title; ?>"
+                                   href="<?= $cmsTree->url; ?>">
+                                    <?php if ($isParent) : ?>
+                                        <i class="hs-icon hs-icon-arrow-left"></i>
+                                    <?php endif; ?>
+                                    <?= $cmsTree->name; ?>
+                                </a>
+                            <? endif; ?>
+
+                        </li>
+                    <?php endif; ?>
+                    <?php if ($savedFilter) : ?>
+                        <li class="<?php echo $isChildren ? "sx-children-cat" : ""; ?>">
+                            <div class="sx-main-text-color g-text-underline--none--hover <?php echo $isCurrent ? "btn btn-primary btn-block" : ""; ?>"
+                                 data-toggle="tooltip" title="<?php echo $title; ?>"
                             >
-                                <?php if($isParent) : ?>
-                                    <i class="hs-icon hs-icon-arrow-left"></i>
-                                <?php endif; ?>
-                                <?= $cmsTree->name; ?>
+                                <?= $savedFilter->shortSeoName; ?>
                             </div>
-                        <? else : ?>
-                            <a class="sx-main-text-color g-text-underline--none--hover"
-                               data-toggle="tooltip" title="<?php echo $title; ?>"
-                               href="<?= $cmsTree->url; ?>">
-                                <?php if($isParent) : ?>
-                                    <i class="hs-icon hs-icon-arrow-left"></i>
-                                <?php endif; ?>
-                                <?= $cmsTree->name; ?>
-                            </a>
-                        <? endif; ?>
+                        </li>
+                    <?php endif; ?>
 
-                    </li>
-                <?php endif; ?>
-                <?php if($savedFilter) : ?>
-                    <li class="<?php echo $isChildren ? "sx-children-cat" : ""; ?>">
-                        <div class="sx-main-text-color g-text-underline--none--hover <?php echo $isCurrent ? "btn btn-primary btn-block" : ""; ?>"
-                           data-toggle="tooltip" title="<?php echo $title; ?>"
-                         >
-                            <?= $savedFilter->shortSeoName; ?>
-                        </div>
-                    </li>
-                <?php endif; ?>
-
-                <?php if($name) : ?>
-                    <li class="<?php echo $isChildren ? "sx-children-cat" : ""; ?>">
-                        <div class="sx-main-text-color g-text-underline--none--hover <?php echo $isCurrent ? "btn btn-primary btn-block" : ""; ?>"
-                           data-toggle="tooltip" title="<?= $name; ?>"
-                         >
-                            <?= $name; ?>
-                        </div>
-                    </li>
-                <?php endif; ?>
-            <?php endforeach; ?>
+                    <?php if ($name) : ?>
+                        <li class="<?php echo $isChildren ? "sx-children-cat" : ""; ?>">
+                            <div class="sx-main-text-color g-text-underline--none--hover <?php echo $isCurrent ? "btn btn-primary btn-block" : ""; ?>"
+                                 data-toggle="tooltip" title="<?= $name; ?>"
+                            >
+                                <?= $name; ?>
+                            </div>
+                        </li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </ul>
         <?php endif; ?>
 
 
 
 
-</div>
-        <? endif; ?>
-
-    
+        </div>
+    <? endif; ?>
 
 
 <? endif; ?>
@@ -200,17 +192,33 @@ CSS
 <div class="sx-col-left-block">
     <div style="display: none;">
         <? endif; ?>
-        <?
-        \skeeks\cms\themes\unify\widgets\filters\assets\FiltersWidgetAsset::register($this);
-        $pjax = \skeeks\cms\widgets\PjaxLazyLoad::begin(); ?>
-        <?php if ($pjax->isPjax) : ?>
-            <? echo $filtersWidget->run(); ?>
-        <?php else : ?>
+        <div class="js-sticky-block" data-has-sticky-header="true" data-start-point=".sx-fast-filters" data-end-point=".sx-footer-wrapper">
+            <div class="sx-filters-left-wrapper js-scrollbar">
+                <?
+                \skeeks\assets\unify\base\UnifyHsStickyBlockAsset::register($this);
+                $this->registerJs(<<<JS
+$(".sx-filters-left-wrapper").css("height", "calc(100vh - " + $(".u-header--sticky-top").height() +  "px)");
+JS
+                );
+                $this->registerCss(<<<CSS
+/*.sx-filters-left-wrapper {
+    overflow-y: auto;
+}*/
+CSS
+                );
 
-        <?php endif; ?>
-        <? $pjax::end(); ?>
+                \skeeks\cms\themes\unify\widgets\filters\assets\FiltersWidgetAsset::register($this);
+                $pjax = \skeeks\cms\widgets\PjaxLazyLoad::begin(); ?>
+                <?php if ($pjax->isPjax) : ?>
+                    <? echo $filtersWidget->run(); ?>
+                <?php else : ?>
 
+                <?php endif; ?>
+                <? $pjax::end(); ?>
+            </div>
+        </div>
         <? if (!\Yii::$app->view->theme->is_allow_filters) : ?>
+
     </div>
 </div>
 <? endif; ?>
